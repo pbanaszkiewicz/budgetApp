9e41db77334e3e1c8d50eae9a5cd230ff81c9693
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2014-02-22 11:22:05 +0100

    Clearing up `conftest.py` a little bit

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,20 +9,24 @@ from budgetApp.app import create_app
 from budgetApp.models import Base
 
 
-# @pytest.fixture(scope="session")
-# def app(request):
 @pytest.yield_fixture(scope="session")
 def app():
+    """
+    Creates a new Flask application for a test duration.  Uses application
+    factory from `budgetApp.app.create_app`.  For a tests using SQLite DB,
+    a `PRAGMA foreign_keys=ON` is issued (it helps with ON DELETE CASCADE).
+    """
     _app = create_app("testingsession", config_object=TestConfig)
 
-    @event.listens_for(Engine, "connect")
-    def set_sqlite_pragma(dbapi_connection, connection_record):
-        """
-        Make SQLite recognize ON DELETE CASCADE.
-        """
-        cursor = dbapi_connection.cursor()
-        cursor.execute("PRAGMA foreign_keys=ON")
-        cursor.close()
+    if _app.config["SQLALCHEMY_DATABASE_URI"].lower().startswith("sqlite"):
+        @event.listens_for(Engine, "connect")
+        def set_sqlite_pragma(dbapi_connection, connection_record):
+            """
+            Make SQLite recognize ON DELETE CASCADE.
+            """
+            cursor = dbapi_connection.cursor()
+            cursor.execute("PRAGMA foreign_keys=ON")
+            cursor.close()
 
     Base.metadata.create_all(bind=_app.engine)
     _app.connection = _app.engine.connect()
@@ -33,20 +37,13 @@ def app():
     # all these scoped sessions configured to use current connection.
     budgetApp.app.DbSession.configure(bind=_app.connection)
 
-    # def teardown_request():
-    #     _app.connection.close()
-    #     Base.metadata.drop_all(bind=_app.engine)
-    # request.addfinalizer(teardown_request)
-
-    # return _app
     yield _app
 
+    # the code after yield statement works as a teardown
     _app.connection.close()
     Base.metadata.drop_all(bind=_app.engine)
 
 
-# @pytest.fixture(scope="function")
-# def session(app, request):
 @pytest.yield_fixture(scope="function")
 def session(app):
     """
@@ -55,25 +52,21 @@ def session(app):
     """
     app.transaction = app.connection.begin()
     app.testing = True
+
+    # pushing new Flask application context for multiple-thread tests to work
     ctx = app.app_context()
     ctx.push()
+
     session = budgetApp.app.DbSession()
 
-    # def teardown():
-    #     app.transaction.close()
-    #     session.close()
-    #     ctx.pop()
-    # request.addfinalizer(teardown)
-
-    # return session
     yield session
 
+    # the code after yield statement works as a teardown
     app.transaction.close()
     session.close()
     ctx.pop()
 
 
-# @pytest.fixture(scope="function")
 @pytest.yield_fixture(scope="function")
 def test_client(app):
     yield app.test_client()
